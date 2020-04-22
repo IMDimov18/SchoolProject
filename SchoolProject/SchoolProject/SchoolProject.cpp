92982ea46dfc530661ddea4d11536dfc3b18e133
IMDimov18 <IMDimov18@codingburgas.bg>
2020-04-22 18:31:09 +0300

    Add function for the main menu and for creating orders

--- SchoolProject/SchoolProject/SchoolProject.cpp
+++ SchoolProject/SchoolProject/SchoolProject.cpp
@@ -1,7 +1,57 @@
-
 #include <iostream>
+#include <string>
+using namespace std;
+struct CAFFE
+{
+    int table;
+    char waiterName;
+    char clientOrder;
+    int time;
+    float bill;
+};
+void createOrder(CAFFE* orders, int& orderCount, int& maxId)
+{
+    CAFFE order;
+    cout << "Table number: ";
+    cin >> order.table;
+    cout << "Waiter name: ";
+    cin >> order.waiterName;
+    cout << "What did the client order?: ";
+    cin >> order.clientOrder;
+    cout << "For how long stayed the client?: ";
+    cin >> order.time;
+    cout << "How much did they pay?: ";
+    cin >> order.bill;
+}
+bool showMainMenu(CAFFE* orders, int& orderCount, int& maxId)
+{
+    int choice;
+    cout << "-------ORDER MENU-------" << endl;
+    cout << "1. Create order" << endl;
+    cout << "2. Show all orders" << endl;
+    cout << "3. Edit order" << endl;
+    cout << "4. Delete order" << endl;
+    cout << "5. Report" << endl;
+    cout << "6. Exit" << endl;
+    cout << "What is your choice?: ";
+    cin >> choice;
+    switch (choice)
+    {
+    case 1:
+        createOrder(orders, orderCount, maxId);
+        break;
+    case 2:
 
+    }
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    int orderCount = 0;
+    int maxId = 1;
+    CAFFE orders[500];
+    bool showMenu;
+    do {
+        showMenu = showMainMenu(orders, orderCount, maxId);
+
+    } while (showMenu);
 }
